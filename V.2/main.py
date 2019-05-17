4d6608bf24704eb3ae1bc4e9bdcdc5fa15d93423
Enrique Mireles <enrique.mireles@udem.edu>
2019-05-16 21:19:57 -0500

    organizar

--- V.2/main.py
+++ V.2/main.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, abort, send_file
+from flask import Flask, render_template, abort, send_file, redirect
 from flask_socketio import SocketIO, disconnect
-import subprocess
+from jinja2 import TemplateNotFound
+import datetime
 import time
 import sys
 import os
+
+currentYear = datetime.datetime.now().strftime("%Y")
+templateData = {
+    'name': 'Vehículo Agrónomo',
+    'copyright': 'Copyright © %s Departamento de Física y Matemáticas | Universidad de Monterrey' % currentYear,
+    'navigation': {
+        'analitica': {
+            'name': 'Analítica',
+            'url': 'analitica.html',
+            'icon': 'fa-tachometer-alt'
+        },
+        'control': {
+            'name': 'Control',
+            'url': 'control.html',
+            'icon': 'fa-cog'
+        }
+    }
+}
+homepage = templateData['navigation']['analitica']
 
 app = Flask(__name__, static_url_path='', static_folder='web/static', template_folder='web/templates')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -14,13 +34,29 @@ app.config['SECRET_KEY'] = 'JORGEMARGAINKEY'
 
 socketio = SocketIO(app)
 
+@app.route('/')
+def index():
+    return redirect(homepage['url'])
+
+@app.route('/<page>')
+def htmlLookup(page):
+    active = None
+    for key, value in templateData['navigation'].items():
+        if value['url'] == page:
+            active = templateData['navigation'][key]
+            break
+    if active:
+        return render_template(page, data=templateData, active=active)
+    else:
+        abort(404)
+
 @app.errorhandler(404)
 def pageNotFound(error):
     return "Not found."
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+@socketio.on('message')
+def handleMessage(message):
+    print(message)
+    
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8000, debug=True)
